f5573e2e87a868734d1e8d61eaf84c8a53f01b70
João Rodrigues <johnnyrod4569@gmail.com>
2025-04-07 17:06:57 +0200

    test: Update test for view of home page (index.html), test passed.

--- ocean_basket/test_views.py
+++ ocean_basket/test_views.py
@@ -1,8 +1,12 @@
+""" This file contains the test cases for the views in the
+ocean_basket app. """
 from django.test import TestCase
 
 
-class TestDjango(TestCase):
+class TestHome(TestCase):
+    """This test case checks the functionality of the home page."""
     def test_get_home_page(self):
-        response = self.client.get('/')
+        """Test the home page loads correctly."""
+        response = self.client.get('')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'ocean_basket/index.html')
+        self.assertTemplateUsed(response, 'index.html')
