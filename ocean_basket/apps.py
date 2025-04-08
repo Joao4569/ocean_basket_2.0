bf8e993832fa4d860f000ac2cfd931072d87e8b0
João Rodrigues <johnnyrod4569@gmail.com>
2025-04-08 14:32:29 +0200

    chore: Clear all linting problems found.

--- ocean_basket/apps.py
+++ ocean_basket/apps.py
@@ -1,6 +1,9 @@
+""" This file contains the configuration for the ocean_basket app. """
 from django.apps import AppConfig
 
 
 class OceanBasketConfig(AppConfig):
+    """ OceanBasketConfig is the configuration class for the
+    ocean_basket app. """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ocean_basket'
--- ocean_basket/test_app_config.py
+++ ocean_basket/test_app_config.py
@@ -1,11 +1,14 @@
+""" This file contains the test cases for the app configuration of
+the Ocean Basket app. """
 from django.apps import apps
 from django.test import TestCase
 from ocean_basket.apps import OceanBasketConfig
 
 
 class TestOceanBasketConfig(TestCase):
+    """Test the OceanBasket app configuration."""
     def test_app_config(self):
-        # Verify the app configuration is correctly set
+        """ Verify the app configuration is correctly set up. """
         self.assertEqual(OceanBasketConfig.name, 'ocean_basket')
         self.assertEqual(
             apps.get_app_config('ocean_basket').name,
